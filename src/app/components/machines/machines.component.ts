3d73758ac68cebb9ca1f065a62c7d09f260fa0b5
scholtyl <laurin.scholtysik@stud.hslu.ch>
2025-02-06 21:01:58 +0100

    machines overview dynamic

--- src/app/components/machines/machines.component.ts
+++ src/app/components/machines/machines.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import { Machine } from '../../models/machine';
+import { MachineService } from '../../services/machine/machine.service';
 
 @Component({
   selector: 'machines-component',
   imports: [],
   templateUrl: './machines.component.html',
-  styleUrl: './machines.component.css'
+  styleUrl: './machines.component.css',
 })
 export class MachinesComponent {
-  
-  selectMachine(id: number): void
-  {
-    console.log(`Selected Machine nr ${id}`)
-  }
+  constructor(private machineService: MachineService) {}
 
+  getMachines(): Machine[] {
+    return this.machineService.getMachines();
+  }
+  selectMachine(id: number): void {
+    console.log(`Selected Machine nr ${id}`);
+  }
 }
--- src/app/services/machine/machine.service.ts
+++ src/app/services/machine/machine.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Machine } from '../../models/machine';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MachineService {
 
   constructor() { }
+
+  getMachines() : Machine[]
+  {
+    let m1 = new Machine(1,"Bauchpresse", new Date("1241-08-01"), 30) 
+    let m2 = new Machine(2,"Beinpresse", new Date("1241-08-01"), 50) 
+    let m3 = new Machine(3,"Brustpresse", new Date("1241-08-01"), 35) 
+    let m4 = new Machine(4,"Lat-Zug", new Date("1241-08-01"), 40) 
+    let m5 = new Machine(5,"Rückenstrecker", new Date("1241-08-01"), 5) 
+    let m6 = new Machine(6,"Schulterpresse", new Date("1241-08-01"), 15) 
+    return [m1, m2, m3, m4, m5, m6]
+  }
 }
